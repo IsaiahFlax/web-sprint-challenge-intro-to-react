e6966fdd82253bf2a1c902404de8a814762db331
angelynaliem <angelynaliem@gmail.com>
2020-08-02 21:33:19 -0400

    Added props to the DOM

--- starwars/src/components/ListOfCharacters.js
+++ starwars/src/components/ListOfCharacters.js
@@ -7,6 +7,19 @@ const ListOfCharacters = ( { character } ) => {
 
     <div>
         <h2>Name: {character.name} </h2>
+        <p>Gender: {character.gender} </p>
+        <p>Eye Color: {character.eye_color} </p>
+        <p>Hair Color: {character.hair_color} </p>
+        <p>Skin Color: {character.skin_color} </p>
+        <p>Height: {character.height} </p>
+        <p>Mass: {character.mass} </p>
+        <p>Birth Year: {character.birth_year} </p>
+        <p>Homeworld: {character.homeworld} </p>
+        <p>Films: {character.films} </p>
+        <p>Vehicles: {character.vehicles}</p>
+        <p>Starships: {character.starships} </p>
+        <p>Species: {character.species} </p>
+
     </div>
 
     )
